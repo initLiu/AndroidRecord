e03be6a9aa935acc2a6b734e32b7afeb692096de
alvin <xianyue_pan@163.com>
2016-08-23 16:55:31 +0800

    delete

--- Fish/src/com/neusoft/fish/observer/BusinessObserver.java
+++
@@ -1,5 +0,0 @@
-package com.neusoft.fish.observer;
-
-public interface BusinessObserver {
-	public abstract void update(Object data);
-}
--- Fish/src/com/neusoft/fish/persist/CallBack.java
+++
@@ -1,5 +0,0 @@
-package com.neusoft.fish.persist;
-
-public interface CallBack {
-	public void onFinish(Object date);
-}
